98e4c412535ff3e84f706682bb314d6905cc5a96
fran <frano4240>
2019-06-06 22:55:12 -0300

    herokuproductionserver

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect(config.DB, { useNewUrlParser: true  }).then(
 );
 var app = express();
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/frontend/public/index.html'));
 //app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
